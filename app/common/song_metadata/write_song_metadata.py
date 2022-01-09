92f2d886a2c15a2c000b6f618d3123f7c365534d
Mai-icy <244095602@qq.com>
2022-01-09 17:20:19 +0800

    :bug: 修复了自定义写入元数据的问题

--- app/common/song_metadata/write_song_metadata.py
+++ app/common/song_metadata/write_song_metadata.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 import os
+import io
 
 import requests
 from mutagen import id3
@@ -9,63 +10,65 @@ from mutagen.flac import FLAC, Picture
 from common.path import LRC_PATH, PLAYLIST_SAVE_PATH
 
 
-def write_flac_metadata(song_path: str, song_info: dict, pic_path=None):
+def write_flac_metadata(song_path: str, song_info: dict, pic_buffer: io.BytesIO = None):
     """
-    Write metadata of flac file.
-
+    Write metadata of flac file. If FLAC has multiple images, clear them all and write only one.
     :param song_path: the path of song
     :param song_info: the info of song
-    :param pic_path: the path of cover picture(jpg)
+    :param pic_buffer: the buffer of cover picture(jpg)
     :return: Return false if the image url connection times out
     """
-    audio = FLAC(song_path)
-    if song_info['songName']:
-        audio["TITLE"] = song_info['songName']
-    if song_info['singer']:
-        audio["ARTIST"] = song_info['singer']
-    if song_info['album']:
-        audio["ALBUM"] = song_info['album']
-    if song_info['trackNumber']:
-        audio["TRACKNUMBER"] = song_info['trackNumber']
-    if song_info['year']:
-        audio["DATE"] = song_info['year']
-    if song_info['genre']:
-        audio["GENRE"] = song_info['genre']
-    # audio["ALBUMARIST"] = song_info['singer']
-    pic = Picture()
-    pic.type = id3.PictureType.COVER_FRONT
-    pic.mime = u"image/jpeg"
-    pic.width = 500
-    pic.height = 500
-    pic.depth = 16  # color depth
-    if pic_path:
-        with open(pic_path, "rb") as f:
-            pic.data = f.read()
-        audio.add_picture(pic)
-    elif song_info["picUrl"]:
-        pic_url = song_info["picUrl"]
-        try:
-            pic_data = requests.get(pic_url, timeout=(4, 8))
-            pic.data = pic_data.content
+    try:
+        audio = FLAC(song_path)
+        if song_info['songName']:
+            audio["TITLE"] = song_info['songName']
+        if song_info['singer']:
+            audio["ARTIST"] = song_info['singer']
+        if song_info['album']:
+            audio["ALBUM"] = song_info['album']
+        if song_info['trackNumber']:
+            audio["TRACKNUMBER"] = song_info['trackNumber']
+        if song_info['year']:
+            audio["DATE"] = song_info['year']
+        if song_info['genre']:
+            audio["GENRE"] = song_info['genre']
+        # audio["ALBUMARIST"] = song_info['singer']
+        pic = Picture()
+        pic.type = id3.PictureType.COVER_FRONT
+        pic.mime = u"image/jpeg"
+        pic.width = 500
+        pic.height = 500
+        pic.depth = 16  # color depth
+        if pic_buffer and pic_buffer.getvalue():
+            pic.data = pic_buffer.getvalue()
+            if len(audio.pictures):
+                audio.clear_pictures()
             audio.add_picture(pic)
-        except requests.exceptions.RequestException:
-            return False
-    audio.save()
-    return True
+        elif song_info["picUrl"]:
+            pic_url = song_info["picUrl"]
+            try:
+                pic_data = requests.get(pic_url, timeout=(4, 8))
+                pic.data = pic_data.content
+                audio.add_picture(pic)
+            except requests.exceptions.RequestException:
+                return False
+        audio.save()
+        return True
+    except Exception:
+        return False
 
 
-def write_mp3_metadata(song_path: str, song_info: dict, pic_path=None) -> bool:
+def write_mp3_metadata(song_path: str, song_info: dict, pic_buffer: io.BytesIO = None) -> bool:
     """
     Write id3 metadata of mp3 file.
-
     :param song_path: the path of song
     :param song_info: the info of song
-    :param pic_path: the path of cover picture(jpg)
+    :param pic_buffer: the buffer of cover picture(jpg)
     :return: Return false if the image url connection times out
     """
     try:
         audio = id3.ID3(song_path)
-    except id3._util.ID3NoHeaderError:
+    except Exception:
         return False
     if song_info["songName"]:
         audio["TIT2"] = id3.TIT2(text=song_info["songName"])
@@ -79,8 +82,8 @@ def write_mp3_metadata(song_path: str, song_info: dict, pic_path=None) -> bool:
         audio["TYER"] = id3.TYER(text=song_info["year"])
     if song_info["genre"]:
         audio["TCON"] = id3.TCON(text=song_info["genre"])
-    if pic_path:
-        audio["APIC:"] = id3.APIC(encoding=3, mime='image/jpeg', type=3, data=open(pic_path, 'rb').read())
+    if pic_buffer and pic_buffer.getvalue():
+        audio["APIC:"] = id3.APIC(encoding=3, mime='image/jpeg', type=3, data=pic_buffer.getvalue())
     elif song_info["picUrl"]:
         pic_url = song_info["picUrl"]
         try:
@@ -125,5 +128,3 @@ def update_md5_to_file(old_md5: str, new_md5: str) -> bool:
 
 if __name__ == "__main__":
     pass
-
-
